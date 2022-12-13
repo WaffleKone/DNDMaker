cc7dd3bdf38eb9f288fc796708fc722f37a18ff0
Jaxon Johnson <88282807+WaffleKone@users.noreply.github.com>
2022-12-13 12:50:23 -0500

    Messed with persistence

--- src/main/java/com/DNDMaker/springdemo/character/Character.java
+++ src/main/java/com/DNDMaker/springdemo/character/Character.java
@@ -7,35 +7,32 @@ import jakarta.persistence.Id;
 
 @Entity
 public class Character {
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long characterId;
+
     private Long playerId;
+
     private CharacterAlignments characterAlignments;
+
     private CharacterClasses characterClasses;
+
     private CharacterRaces characterRaces;
+
+    public Character() {
+
+    }
 
     public Character(Long playerId) {
         this.playerId = playerId;
     }
 
-    public Long getCharacterId() {
-        return characterId;
-    }
-
-    public Long getPlayerId() {
-        return playerId;
-    }
-
-    public CharacterAlignments getCharacterAlignments() {
-        return characterAlignments;
-    }
-
-    public CharacterClasses getCharacterClasses() {
-        return characterClasses;
-    }
-
-    public CharacterRaces getCharacterRaces() {
-        return characterRaces;
+    @Override
+    public String toString() {
+        return "Character{" +
+                "characterId=" + characterId +
+                ", playerId=" + playerId +
+                '}';
     }
 }
---
+++ src/test/java/com/DNDMaker/springdemo/CharacterPersistenceTests.java
@@ -0,0 +1,30 @@
+package com.DNDMaker.springdemo;
+
+import com.DNDMaker.springdemo.character.Character;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+public class CharacterPersistenceTests {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(CharacterPersistenceTests.class);
+
+    @Autowired
+    private CharacterRepository characterRepository;
+
+    @AfterEach
+    public void report() {
+        LOGGER.info("Tests complete.");
+        LOGGER.info("Repository Count: {}", characterRepository.count());
+    }
+
+    @Test
+    public void saveTest() {
+        Character character = new Character();
+        characterRepository.save(character);
+    }
+}
